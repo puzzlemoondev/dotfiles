7e8ae95267d3dc864c820cbf879c6b7c0e73d909
Sean Ling <aegis965@tutamail.com>
2020-08-25 02:59:33 +0800

    fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,32 @@
+# keybind
 set -U fish_key_bindings fish_vi_key_bindings
 
+# plugins
 set -g pure_symbol_prompt "λ"
 set -g pure_symbol_reverse_prompt "ƛ"
 
 set -g async_prompt_functions 'fish_right_prompt'
 
+# programs
 eval (direnv hook fish)
+
+# greeting
+set o (set_color red)
+set m (set_color f70)
+set i (set_color yellow)
+
+echo '                 '$o'___
+  ___======____='$m'-'$i'-'$m'-='$o')
+/T            \_'$i'--='$m'=='$o')
+[ \ '$m'('$i'O'$m')   '$o'\~    \_'$i'-='$m'='$o')
+ \      / )J'$m'~~    '$o'\\'$i'-='$o')
+  \\\\___/  )JJ'$m'~'$i'~~   '$o'\)
+   \_____/JJJ'$m'~~'$i'~~    '$o'\\
+   '$m'/ '$o'\  '$i', \\'$o'J'$m'~~~'$i'~~     '$m'\\
+  (-'$i'\)'$o'\='$m'|'$i'\\\\\\'$m'~~'$i'~~       '$m'L_'$i'_
+  '$m'('$o'\\'$m'\\)  ('$i'\\'$m'\\\)'$o'_           '$i'\=='$m'__
+   '$o'\V    '$m'\\\\'$o'\) =='$m'=_____   '$i'\\\\\\\\'$m'\\\\
+          '$o'\V)     \_) '$m'\\\\'$i'\\\\JJ\\'$m'J\)
+                      '$o'/'$m'J'$i'\\'$m'J'$o'T\\'$m'JJJ'$o'J)
+                      (J'$m'JJ'$o'| \UUU)
+                       (UU)'(set_color normal)
