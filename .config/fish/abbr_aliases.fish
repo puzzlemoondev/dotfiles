0d49e26192fe65094ddb1ec2501cc50f845bbda3
Sean Ling <sean_ling@kiipo.com>
2020-08-25 18:59:40 +0800

    fish

--- .config/fish/abbr_aliases.fish
+++ .config/fish/abbr_aliases.fish
@@ -1,18 +1,23 @@
 # replacements
 alias more='less'
 alias vim='nvim'
+alias ls='lsd --group-dirs first'
 
 # default arguments
 alias diff='diff --color=auto'
 alias grep='grep --color=auto'
 alias ip='ip --color=auto'
-alias ls='lsd --group-dirs first'
 alias df='df -h'
 alias free='free -m'
+alias mkdir='mkdir -pv'
 alias yay='yay --nocleanmenu --nodiffmenu --noeditmenu --removemake --cleanafter'
 alias pacwall='pacwall -b "#323D43" -s "#D8CAAC22" -d "#E68183AA" -e "#89BEBAAA" -p "#A7C080AA" -f "#D3A0BCAA" -u "#D9BB80AA" -r 0.6 -o {$HOME}/Pictures/walls/pacwall.png'
 
 # coreutils
+alias cd='cd && ls'
+
+abbr -ag c 'clear'
+
 abbr -ag l 'ls -l'
 abbr -ag la 'ls -a'
 abbr -ag lla 'ls -la'
@@ -24,6 +29,7 @@ abbr -ag dud 'du -d 1 -h'
 abbr -ag duf 'du -sh *'
 abbr -ag fdd 'fd -t d'
 abbr -ag fdf 'fd -t f'
+abbr -ag map 'xargs -n1'
 
 # yadm
 abbr -ag ya 'yadm add'
@@ -63,9 +69,14 @@ abbr -ag asmr 'adb-sync -R /sdcard/Music/ ~/Music/'
 abbr -ag asmrd 'adb-sync -R -d /sdcard/Music/ ~/Music/'
 
 # misc
+abbr -ag x 'extract'
+abbr -ag v 'vifm'
+abbr -ag e 'nvim'
+abbr -ag q 'exit'
 abbr -ag aw 'awman'
 abbr -ag ur 'sudo reflector -p http -p https -l 30 -n 20 --sort rate --save /etc/pacman.d/mirrorlist --verbose'
 abbr -ag par 'prettyping archlinux.org'
 abbr -ag bat 'upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep -E "state|to\ full|percentage"'
 abbr -ag clock 'tty-clock -s -c -D -C 6'
-
+abbr -ag fconf 'vim ~/.config/fish/config.fish'
+abbr -ag aconf 'vim ~/.config/fish/abbr_aliases.fish; and for a in (abbr -l); abbr -e $a; end; and source ~/.config/fish/abbr_aliases.fish'
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,3 +10,9 @@ set -g pure_symbol_prompt "λ"
 set -g pure_symbol_reverse_prompt "ƛ"
 
 set -g async_prompt_functions 'fish_right_prompt'
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /opt/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
+
