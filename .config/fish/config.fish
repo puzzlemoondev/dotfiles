cfbd7a685e03c57fc18a4ad49b5b899f03ac1981
Sean Ling <aegis965@tutamail.com>
2020-09-25 03:45:10 +0800

    spectrwm bindings

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -293,9 +293,8 @@ end
 if type -q barva; and test -n "$DISPLAY" -a (id -u) != 0 -a (pgrep barva | wc -l) = 0 
     set -x BARVA_SOURCE (/usr/share/barva/pa-get-default-monitor.sh)
     set -x BARVA_BG "#$bg0"
-    set -x BARVA_TARGET "#$bg1"
+    set -x BARVA_TARGET "#$bg2"
 
     barva | /usr/share/barva/to-all-ttys.sh > /dev/null 2>&1 &
-    disown (jobs -lp)
 end
 # }}}
