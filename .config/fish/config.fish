911a591f78f51297fba223a95bb2d23216b89cf6
Sean Ling <sean_ling@kiipo.com>
2020-10-05 16:01:47 +0800

    systemctl

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -121,7 +121,7 @@ if status --is-interactive
     abbr -ag map 'xargs -n1'
 
     ## Systemd
-    abbr -ag sc 'systemctl'
+    abbr -ag sc 'sudo systemctl'
     abbr -ag scu 'systemctl --user'
     abbr -ag jor 'journalctl'
     abbr -ag jour 'journalctl --user'
@@ -231,16 +231,6 @@ function mkc --wraps mkdir -d 'Create a directory and cd into it'
         cd $argv[(count $argv)]
         return
     end
-    end
-end
-
-function systemctl -d 'wraps privileged and user systemctl commands' -w systemctl
-    set -l user_commands list-units list-unit-files list-jobs list-timers list-sockets list-dependencies list-machines is-active is-enabled is-failed status show help get-default show-environment cat
-
-    if contains -- --user $argv; or contains -- $argv[1] $user_commands
-        command systemctl $argv
-    else
-        command sudo systemctl $argv
     end
 end
 
