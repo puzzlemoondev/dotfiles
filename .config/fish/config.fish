de12e656acc268c761b012f1f58493ef371e6bb6
Sean Ling <sean_ling@kiipo.com>
2021-03-12 15:50:18 +0800

    rm conda

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,5 @@
 #{{{ Environment Variables
-set -gx PATH ~/.local/bin /usr/local/bin /opt/miniconda3/bin $PATH
+set -gx PATH ~/.local/bin /usr/local/bin $PATH
 set -gx LANG en_US.UTF-8
 set -gx LANGUAGE en_US.UTF-8
 set -gx LC_ALL en_US.UTF-8
