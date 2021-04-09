01dc51f1497d8b675be0ef35d2959fc845042119
Sean Ling <sean_ling@kiipo.com>
2021-04-09 16:39:02 +0800

    fish indent

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -434,40 +434,40 @@ end
 
 function extract -d 'Expand or extract bundled & compressed files'
     if test -f $argv
-	switch $argv
-	    case \*.rar
-		unrar x $argv
-	    case \*.zip
-		unzip $argv
-	    case \*.tar.bz2
-		tar xvjf $argv
-	    case \*.tar.gz
-		tar xvzf $argv
-	    case \*.bz2
-		bunzip2 $argv
-	    case \*.gz
-		gunzip $argv
-	    case \*.tar
-		tar xvf $argv
-	    case \*.tbz2
-		tar xvjf $argv
-	    case \*.tgz
-		tar xvzf $argv
-	    case \*.Z
-		uncompress $argv
-	    case \*.7z
-		7z x $argv
-	    case '*'
-		echo "unknown extension"
-	end
+        switch $argv
+            case \*.rar
+                unrar x $argv
+            case \*.zip
+                unzip $argv
+            case \*.tar.bz2
+                tar xvjf $argv
+            case \*.tar.gz
+                tar xvzf $argv
+            case \*.bz2
+                bunzip2 $argv
+            case \*.gz
+                gunzip $argv
+            case \*.tar
+                tar xvf $argv
+            case \*.tbz2
+                tar xvjf $argv
+            case \*.tgz
+                tar xvzf $argv
+            case \*.Z
+                uncompress $argv
+            case \*.7z
+                7z x $argv
+            case '*'
+                echo "unknown extension"
+        end
     else
-	echo "Could not extract $argv"
+        echo "Could not extract $argv"
     end
 end
 
 function fish_greeting
     if type -q fish_logo
-	fish_logo $red $orange $yellow
+        fish_logo $red $orange $yellow
     end
 end
 #}}}
