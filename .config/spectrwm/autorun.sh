213a240f34610212919344aace356e5d8f5ca2b1
Sean Ling <sean_ling@kiipo.com>
2021-04-21 12:06:37 +0800

    autorun

---
+++ .config/spectrwm/autorun.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+numlockx on &
+xautolock -locker 'betterlockscreen -l dimblur' -time 10 &
+nitrogen --restore &
+redshift -l 25.04:121.53 -g 1.0:0.8:0.7 &
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+gnome-keyring-daemon --start --components=pkcs11,secrets &
+picom -b &
+pulseeffects --gapplication-service &
+start-barva &
+fcitx &
