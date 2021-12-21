34e69095c3732fce3c4e68c88fdd8f8fae9cffd0
PuzzleMoon <32855965+puzzlemoondev@users.noreply.github.com>
2021-12-21 17:52:31 +0800

    kde

--- .config/spectrwm/autorun.sh
+++ .config/spectrwm/autorun.sh
@@ -1,21 +1,4 @@
 #!/bin/bash
 
-nitrogen --restore &
-redshift -l 25.04:121.53 -g 1.0:0.8:0.7 &
-
-numlockx on &
-if [[ $USER != *"sean"* ]]; then
-	xset 300
-	xss-lock -l -- locker &
-fi
-
-/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-gnome-keyring-daemon --start --components=pkcs11,secrets &
-
-picom -b &
-easyeffects --gapplication-service &
-
-start-barva &
-fcitx &
-
-nm-applet &
+picom -b
+nitrogen --restore
--- .local/share/konsole/Everforest.profile
+++ .local/share/konsole/Everforest.profile
@@ -5,8 +5,12 @@ Font=VictorMono Nerd Font,12,-1,5,50,0,0,0,0,0
 UseFontLineChararacters=true
 
 [General]
-DimWhenInactive=true
+DimWhenInactive=false
 Name=Everforest
 Parent=FALLBACK/
+ShowTerminalSizeHint=false
 TerminalCenter=true
 TerminalMargin=24
+
+[Scrolling]
+ScrollBarPosition=2
