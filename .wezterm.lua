aa6e3605919b104d7ea1ab05e460c9d0c191ed43
PuzzleMoon <32855965+puzzlemoondev@users.noreply.github.com>
2023-11-22 16:22:03 +0800

    wezterm font

--- .wezterm.lua
+++ .wezterm.lua
@@ -14,7 +14,7 @@ end
 
 config.color_scheme = 'Everforest Dark (Gogh)'
 
-config.font = wezterm.font 'VictorMono NerdFont'
+config.font = wezterm.font 'Victor Mono'
 config.font_size = 16.0
 
 config.window_padding = {
